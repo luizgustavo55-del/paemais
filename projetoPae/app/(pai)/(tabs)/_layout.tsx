d84bd041417cc0812a63cbf8d3b03209a53120c0
luizGustavo55-del <pereiraluizgustavo55@gmail.com>
2026-03-22 13:01:37 -0400

    mudanças no header

--- projetoPae/app/(pai)/(tabs)/_layout.tsx
+++ projetoPae/app/(pai)/(tabs)/_layout.tsx
@@ -1,11 +1,15 @@
-import { Stack, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
 
 export default function PaiTabs() {
   return (
-    <Tabs>
+    <Tabs
+     // screenOptions={{
+     //   headerShown: false, 
+      //}}
+    >
       <Tabs.Screen name="menu" options={{ title: "Menu" }} />
       <Tabs.Screen name="comunidade" options={{ title: "Comunidade" }} />
       <Tabs.Screen name="dicas" options={{ title: "Dicas" }} />
-    </Tabs> 
+    </Tabs>
   );
 }
